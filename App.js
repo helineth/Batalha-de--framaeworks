a62fe38810eee6ace7170c8313cdd906fd001ea7
Bola de Berlim <76258195+helineth@users.noreply.github.com>
2023-05-25 10:30:41 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div class="container">
+      <h1>Batalha dos frameworks</h1>
+      <img src="https://i.imgur.com/2Rxt3DX.png" alt="" />
+      <h2>VueJS <span>vs</span> ReactJS</h2>
+      <div class="frameworks">
+        <div class="back-react"></div>
+        <div class="front-vue"></div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
